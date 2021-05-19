fb3df7db881c848ca96df8b872e25b17b2b46422
YYnvidia <yydeeplearning@gmail.com>
2021-05-20 01:13:30 +0900

    optimize for school HPC

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import os
-<<<<<<< HEAD
 import sys
-=======
->>>>>>> b7a695652455c758bddc03a1de6679fdf51a661b
 
 from Steam_spider import Spider
 from Info_extractor import Extractor
@@ -12,13 +9,8 @@ __author__ =  "YU Yang"
 
 def main():
     os.chdir(os.getcwd())
-<<<<<<< HEAD
-    appid = sys.argv[1]
-=======
     # appid = input("Please input the appid here: ")
-    appid = '275850' # No Man's Sky
-
->>>>>>> b7a695652455c758bddc03a1de6679fdf51a661b
+    appid = sys.argv[1]
 
     SteamCollector = Spider(appid)
     source_dict, _ = SteamCollector.source()
@@ -28,23 +20,14 @@ def main():
     print("Review analysis complete!")
     
     try:
-<<<<<<< HEAD
         os.chdir('{}_data'.format(appid))
     except:
         os.mkdir('{}_data'.format(appid))
         os.chdir('{}_data'.format(appid))
 
-=======
-        os.chdir('data')
-    except:
-        os.mkdir('data')
-        os.chdir('data')
-
     with open('{} Info'.format(appid),'w+',encoding='utf-8') as file_info:
         file_info.write("positive: {}, negative: {}, total: {}, non_en_rates: {}".format(positive,negative,total,non_en_rates))
     
-    
->>>>>>> b7a695652455c758bddc03a1de6679fdf51a661b
     SteamWriter = Writer(appid,record_dict)
     SteamWriter.write_all()
     
